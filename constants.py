f862d736974ec8c7e19b883313b480480107542c
Nikilite <109990495+0xNikilite@users.noreply.github.com>
2026-02-12 16:10:58 +0100

    last.fm api key from config.ini
    instead of hardcoding it

--- constants.py
+++ constants.py
@@ -1,15 +1,11 @@
-import base64
 import os
-
 
 _cwd = os.getcwd()
 
-_EGGPLANT = b'ZjZkN2YyMWMwMDQwZjhjYjkxOTljNTQ0ZmFmM2I5ZWE='
-BABA_GHANOUJ = base64.b64decode(_EGGPLANT).decode('ascii')
 LAST_SUBMITTED_LISTEN_FILE = os.path.join(_cwd, 'last_submitted_listen')
 LASTFM_API_ROOT = 'http://ws.audioscrobbler.com/2.0/'
 LASTFM_DATA_ROOT = os.path.join(_cwd, 'lastfm_data')
-LATEST_SCHEMA_VERSION = 1  # keep in sync with schema.sql
+LATEST_SCHEMA_VERSION = 1
 LISTENBRAINZ_API_ROOT = 'https://api.listenbrainz.org/1/'
 SCROBBLE_FIXES_ROOT = os.path.join(_cwd, 'scrobble_fixes')
 SCROBBLES_DB_FILE = os.path.join(_cwd, 'scrobbles.db')
